c2d9847eae2abffe20f3d9c7cc9c7c299281f879
mengcc_y7000p <1743122693@qq.com>
2023-02-26 16:10:57 +0800

    Agent peer with web and mongodb first commit

---
+++ src/main/java/hnu/csee/mengcc/agent_peer/AgentPeerApplication.java
@@ -0,0 +1,13 @@
+package hnu.csee.mengcc.agent_peer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgentPeerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgentPeerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hnu/csee/mengcc/agent_peer/AgentPeerApplicationTests.java
@@ -0,0 +1,13 @@
+package hnu.csee.mengcc.agent_peer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgentPeerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
