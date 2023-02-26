a3491600b0e43b0eeb61e694f7a78ed0dcac1e5b
mengcc666 <1743122693@qq.com>
2023-02-26 17:54:10 +0800

    Add Todo.java

---
+++ src/main/java/hnu/csee/mengcc/agent_peer/todo_package/Todo.java
@@ -0,0 +1,32 @@
+package hnu.csee.mengcc.agent_peer.todo_package;
+
+public class Todo {
+
+  private String id;
+  private String desc;
+  private boolean completed;
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getDesc() {
+    return desc;
+  }
+
+  public void setDesc(String desc) {
+    this.desc = desc;
+  }
+
+  public boolean isCompleted() {
+    return completed;
+  }
+
+  public void setCompleted(boolean completed) {
+    this.completed = completed;
+  }
+}
