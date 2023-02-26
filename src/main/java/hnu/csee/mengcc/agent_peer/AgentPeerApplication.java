ae86e1ab76116a25e0ebfd6cb210dfd44f07f879
mengcc_y7000p <1743122693@qq.com>
2023-02-26 16:20:12 +0800

    Carry todo from old to new(web and mongodb)

--- src/main/java/hnu/csee/mengcc/agent_peer/AgentPeerApplication.java
+++ src/main/java/hnu/csee/mengcc/agent_peer/AgentPeerApplication.java
@@ -1,13 +1,40 @@
 package hnu.csee.mengcc.agent_peer;
 
+import hnu.csee.mengcc.agent_peer.Functions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class AgentPeerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AgentPeerApplication.class, args);
 	}
+	@GetMapping(value = "/hello")
+  public String hello(
+    @RequestParam(value = "name", defaultValue = "World") String name
+  ) {
+    return String.format("Hello %s!", name);
+  }
+
+  @GetMapping(value = "/startNetwork")
+  public void startNetwork() throws InterruptedException {
+    String[] commands = { "./network.sh", "up" };
+    String path = "/root/workspace/fabric-samples/test-network";
+    Functions f = new Functions();
+    f.InvokeCommandAtSpecifiedPath(commands, path);
+  }
+
+  @GetMapping(value = "/stopNetwork")
+  public void stopNetwork() throws InterruptedException {
+    String[] commands = { "./network.sh", "down" };
+    String path = "/root/workspace/fabric-samples/test-network";
+    Functions f = new Functions();
+    f.InvokeCommandAtSpecifiedPath(commands, path);
+  }
 
 }
