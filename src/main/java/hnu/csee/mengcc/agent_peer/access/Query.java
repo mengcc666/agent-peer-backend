6a382fd1531d619d61465d7baeb082565d70ec96
mengcc <1743122693@qq.com>
2023-03-02 10:18:05 +0800

    Add import statements

--- src/main/java/hnu/csee/mengcc/agent_peer/access/Query.java
+++ src/main/java/hnu/csee/mengcc/agent_peer/access/Query.java
@@ -1,5 +1,8 @@
 package hnu.csee.mengcc.agent_peer.access;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class Query {
     @GetMapping("/query")
