70b7981cf58dc5df5a39e9f3daca92bb79763bb9
mengcc <1743122693@qq.com>
2023-03-06 17:33:09 +0800

    Add query api (not finished)

--- src/main/java/hnu/csee/mengcc/agent_peer/access/Query.java
+++ src/main/java/hnu/csee/mengcc/agent_peer/access/Query.java
@@ -2,11 +2,36 @@ package hnu.csee.mengcc.agent_peer.access;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import org.hyperledger.fabric.gateway.Contract;
+import org.hyperledger.fabric.gateway.Gateway;
+import org.hyperledger.fabric.gateway.Network;
+import org.hyperledger.fabric.gateway.Wallet;
+import org.hyperledger.fabric.gateway.Wallets;
 
 @RestController
 public class Query {
+    static {
+        System.setProperty("org.hyperledger.fabric.sdk.service_discovery.as_localhost", "true");
+    }
+
+    // helper function for getting connected to the gateway
+    public static Gateway connect() throws Exception {
+        // Load a file system based wallet for managing identities.
+        Path walletPath = Paths.get("wallet");
+        Wallet wallet = Wallets.newFileSystemWallet(walletPath);
+        // load a CCP
+        Path networkConfigPath = Paths.get("..", "..", "test-network", "organizations", "peerOrganizations",
+                "org1.example.com", "connection-org1.yaml");
+
+        Gateway.Builder builder = Gateway.createBuilder();
+        builder.identity(wallet, "appUser").networkConfig(networkConfigPath).discovery(true);
+        return builder.connect();
+    }
+
     @GetMapping("/query")
-    public String query(){
+    public String query() {
         return String.format("Invoke query()");
     }
 }
