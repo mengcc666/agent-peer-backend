190fe8ebe4ba0118435ccff6e066d07c092d21ac
mengcc <1743122693@qq.com>
2023-03-07 13:47:32 +0800

    Import List and Param

--- src/main/java/hnu/csee/mengcc/agent_peer/todo_package/TodoRepository.java
+++ src/main/java/hnu/csee/mengcc/agent_peer/todo_package/TodoRepository.java
@@ -2,6 +2,9 @@ package hnu.csee.mengcc.agent_peer.todo_package;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import java.util.List;
+import org.springframework.data.repository.query.Param;
+
 
 @RepositoryRestResource(collectionResourceRel = "todo", path = "todo")
 public interface TodoRepository extends MongoRepository<Todo, String> {
