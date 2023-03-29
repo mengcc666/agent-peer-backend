50868f4a828b4260eb6ab1f899848fdbe329dbaf
mengcc <1743122693@qq.com>
2023-03-29 10:45:27 +0800

    Delete package todo, user

--- src/main/java/hnu/csee/mengcc/agentpeer/response/UserReponse.java
+++
@@ -1,5 +0,0 @@
-package hnu.csee.mengcc.agentpeer.response;
-
-public class UserReponse {
-    public String message;
-}
--- src/main/java/hnu/csee/mengcc/agentpeer/user/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package hnu.csee.mengcc.agentpeer.user;
-
-import java.util.List;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.repository.query.Param;
-
-public interface UserRepository extends MongoRepository<User, String> {
-  List<User> findByEmail(@Param("email") String email);
-}
