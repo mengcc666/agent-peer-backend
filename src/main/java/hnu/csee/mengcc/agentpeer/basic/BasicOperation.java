1359807a12c4d4ca0490fac6e9c7cc14ee0c1cbb
mengcc_y7000p <1743122693@qq.com>
2023-04-04 20:43:02 +0800

    Reorganize the folder structure

---
+++ src/main/java/hnu/csee/mengcc/agentpeer/basic/BasicOperation.java
@@ -0,0 +1,108 @@
+package hnu.csee.mengcc.agentpeer.basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BasicOperation {
+
+  @GetMapping("user")
+  public String helloUser() {
+      return "Hello User";
+  }
+
+  @GetMapping("admin")
+  public String helloAdmin() {
+      return "Hello Admin";
+  }
+
+  @GetMapping(value = "/hello")
+  public String hello(
+    @RequestParam(value = "name", defaultValue = "World") String name
+  ) {
+    return String.format("Hello %s", name);
+  }
+
+  @GetMapping(value = "/startNetwork")
+  public void startNetwork() throws InterruptedException {
+    String[] commands = { "./network.sh", "up" };
+    String path = "/root/workspace/fabric-samples/test-network";
+    FunctionsRepo f = new FunctionsRepo();
+    f.InvokeCommandAtSpecifiedPath(commands, path);
+  }
+
+  @GetMapping(value = "/initCA")
+  public void initCA() throws InterruptedException {
+    String[] commands = { "./network.sh", "up", "-ca" };
+    String path = "/root/workspace/fabric-samples/test-network";
+    FunctionsRepo f = new FunctionsRepo();
+    f.InvokeCommandAtSpecifiedPath(commands, path);
+  }
+
+  @GetMapping(value = "/stopNetwork")
+  public void stopNetwork() throws InterruptedException {
+    String[] commands = { "./network.sh", "down" };
+    String path = "/root/workspace/fabric-samples/test-network";
+    FunctionsRepo f = new FunctionsRepo();
+    f.InvokeCommandAtSpecifiedPath(commands, path);
+  }
+
+  @GetMapping(value = "/createChannel")
+  public void createChannel(
+    @RequestParam(value = "name", defaultValue = "mychannel") String channelName
+  ) throws InterruptedException {
+    //./network.sh up createChannel -c mychannel -ca
+    String[] commands = {
+      "./network.sh",
+      "createChannel",
+      "-c",
+      channelName,
+      "-ca",
+    };
+    String path = "/root/workspace/fabric-samples/test-network";
+    FunctionsRepo f = new FunctionsRepo();
+    f.InvokeCommandAtSpecifiedPath(commands, path);
+  }
+
+  @GetMapping(value = "/getBlockchainNetworkInfo")
+  public void getBlockchainNetworkInfo() throws InterruptedException {
+    String[] commands = { "docker", "ps", "-a" };
+    String path = "/root";
+    FunctionsRepo f = new FunctionsRepo();
+    f.InvokeCommandAtSpecifiedPath(commands, path);
+  }
+
+  @GetMapping(value = "/deployChaincode")
+  public void deployChaincode(
+    @RequestParam(
+      value = "chaincodeName",
+      defaultValue = "basic"
+    ) String chaincodeName,
+    @RequestParam(
+      value = "chaincodePath",
+      defaultValue = "../asset-transfer-basic/chaincode-javascript/"
+    ) String chaincodePath,
+    @RequestParam(
+      value = "chaincodeLanguage",
+      defaultValue = "javascript"
+    ) String chaincodeLanguage
+  ) throws InterruptedException {
+    //./network.sh deployCC -ccn basic -ccp ../asset-transfer-basic/chaincode-javascript/ -ccl javascript
+    String[] commands = {
+      "./network.sh",
+      "deployCC",
+      "-ccn",
+      chaincodeName,
+      "-ccp",
+      chaincodePath,
+      "-ccl",
+      chaincodeLanguage,
+    };
+    String path = "/root/workspace/fabric-samples/test-network";
+    FunctionsRepo f = new FunctionsRepo();
+    f.InvokeCommandAtSpecifiedPath(commands, path);
+  }
+}
---
+++ src/main/java/hnu/csee/mengcc/agentpeer/chaincode/invokeChaincode.java
@@ -0,0 +1,8 @@
+package hnu.csee.mengcc.agentpeer.chaincode;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class invokeChaincode {
+    
+}
