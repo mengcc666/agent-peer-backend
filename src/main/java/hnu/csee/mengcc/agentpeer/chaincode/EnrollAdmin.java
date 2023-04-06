7dfaab6501f6e9bc9e5ccc859bc60e4b0b8bc73d
mengcc_y7000p <1743122693@qq.com>
2023-04-06 23:12:15 +0800

    Add try catch around all

--- src/main/java/hnu/csee/mengcc/agentpeer/chaincode/EnrollAdmin.java
+++ src/main/java/hnu/csee/mengcc/agentpeer/chaincode/EnrollAdmin.java
@@ -21,50 +21,50 @@ import org.hyperledger.fabric_ca.sdk.HFCAClient;
 public class EnrollAdmin {
 
   public static void main(String[] args) throws Exception {
-    // Create a CA client for interacting with the CA.
-    Properties props = new Properties();
-    props.put(
-      "pemFile",
-      "/root/workspace/fabric-samples/test-network/organizations/peerOrganizations/org1.example.com/ca/ca.org1.example.com-cert.pem"
-    );
-    props.put("allowAllHostNames", "true");
-    try{
+    try {
+      // Create a CA client for interacting with the CA.
+      Properties props = new Properties();
+      props.put(
+        "pemFile",
+        "/root/workspace/fabric-samples/test-network/organizations/peerOrganizations/org1.example.com/ca/ca.org1.example.com-cert.pem"
+      );
+      props.put("allowAllHostNames", "true");
       HFCAClient caClient = HFCAClient.createNewInstance(
         "https://localhost:7054",
         props
       );
-    }catch (Exception e) {
+      CryptoSuite cryptoSuite = CryptoSuiteFactory.getDefault().getCryptoSuite();
+      caClient.setCryptoSuite(cryptoSuite);
+  
+      // Create a wallet for managing identities
+      Wallet wallet = Wallets.newFileSystemWallet(Paths.get("wallet"));
+  
+      // Check to see if we've already enrolled the admin user.
+      if (wallet.get("admin") != null) {
+        System.out.println(
+          "An identity for the admin user \"admin\" already exists in the wallet"
+        );
+        return;
+      }
+  
+      // Enroll the admin user, and import the new identity into the wallet.
+      final EnrollmentRequest enrollmentRequestTLS = new EnrollmentRequest();
+      enrollmentRequestTLS.addHost("localhost");
+      enrollmentRequestTLS.setProfile("tls");
+      Enrollment enrollment = caClient.enroll(
+        "admin",
+        "adminpw",
+        enrollmentRequestTLS
+      );
+      Identity user = Identities.newX509Identity("Org1MSP", enrollment);
+      wallet.put("admin", user);
+      System.out.println(
+        "Successfully enrolled user \"admin\" and imported it into the wallet"
+      );
+    } catch (Exception e) {
       System.err.println(e);
     }
 
 
-    CryptoSuite cryptoSuite = CryptoSuiteFactory.getDefault().getCryptoSuite();
-    caClient.setCryptoSuite(cryptoSuite);
-
-    // Create a wallet for managing identities
-    Wallet wallet = Wallets.newFileSystemWallet(Paths.get("wallet"));
-
-    // Check to see if we've already enrolled the admin user.
-    if (wallet.get("admin") != null) {
-      System.out.println(
-        "An identity for the admin user \"admin\" already exists in the wallet"
-      );
-      return;
-    }
-
-    // Enroll the admin user, and import the new identity into the wallet.
-    final EnrollmentRequest enrollmentRequestTLS = new EnrollmentRequest();
-    enrollmentRequestTLS.addHost("localhost");
-    enrollmentRequestTLS.setProfile("tls");
-    Enrollment enrollment = caClient.enroll(
-      "admin",
-      "adminpw",
-      enrollmentRequestTLS
-    );
-    Identity user = Identities.newX509Identity("Org1MSP", enrollment);
-    wallet.put("admin", user);
-    System.out.println(
-      "Successfully enrolled user \"admin\" and imported it into the wallet"
-    );
   }
 }
