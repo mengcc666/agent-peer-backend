ffcafc223aa7f637f01f4bb6cf2444978fb81647
mengcc666 <55180978+mengcc666@users.noreply.github.com>
2023-04-12 18:17:19 +0800

    Leave lab

---
+++ src/main/java/hnu/csee/mengcc/agentpeer/connecttofabric/Connect.java
@@ -0,0 +1,46 @@
+package hnu.csee.mengcc.agentpeer.connecttofabric;
+
+import org.hyperledger.fabric.gateway.Gateway;
+import org.hyperledger.fabric.gateway.Wallet;
+import org.hyperledger.fabric.gateway.Wallets;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * @Author Mengcc
+ * @Date 2023/4/12 10:16
+ * @Version 1.0
+ */
+
+@RestController
+public class Connect {
+
+    // helper function for getting connected to the gateway
+    public static Gateway connect() throws Exception{
+        // Load a file system based wallet for managing identities.
+        Path walletPath = Paths.get("wallet");
+        Wallet wallet = Wallets.newFileSystemWallet(walletPath);
+        // load a CCP
+        Path networkConfigPath = Paths.get("..", "..", "test-network", "organizations", "peerOrganizations", "org1.example.com", "connection-org1.yaml");
+
+        Gateway.Builder builder = Gateway.createBuilder();
+        builder.identity(wallet, "appUser").networkConfig(networkConfigPath).discovery(true);
+        return builder.connect();
+    }
+
+    @GetMapping("connect/invokeChaincode")
+    public String invokeChaincode(){
+        try {
+            EnrollAdmin.enrollAdmin();
+//            RegisterUser.main(null);
+        } catch (Exception e) {
+            System.err.println(e);
+        }
+
+        String str="invoke chaincode function";
+        return str;
+    }
+}
