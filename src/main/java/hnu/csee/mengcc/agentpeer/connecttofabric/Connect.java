1adad7e7992a64c386f8268124e76a55e023d500
mengcc666 <55180978+mengcc666@users.noreply.github.com>
2023-04-13 15:50:16 +0800

    Add connect code

--- src/main/java/hnu/csee/mengcc/agentpeer/connecttofabric/Connect.java
+++ src/main/java/hnu/csee/mengcc/agentpeer/connecttofabric/Connect.java
@@ -21,13 +21,14 @@ public class Connect {
     // helper function for getting connected to the gateway
     public static Gateway connect() throws Exception{
         // Load a file system based wallet for managing identities.
+        String peerAddress="101.34.79.126:7051";
         Path walletPath = Paths.get("wallet");
         Wallet wallet = Wallets.newFileSystemWallet(walletPath);
         // load a CCP
-        Path networkConfigPath = Paths.get("..", "..", "test-network", "organizations", "peerOrganizations", "org1.example.com", "connection-org1.yaml");
+        Path networkConfigPath = Paths.get( "..", "organizations", "peerOrganizations", "org1.example.com", "connection-org1.yaml");
 
         Gateway.Builder builder = Gateway.createBuilder();
-        builder.identity(wallet, "appUser").networkConfig(networkConfigPath).discovery(true);
+        builder.identity(wallet, "appUser").networkConfig(networkConfigPath).discovery(false);
         return builder.connect();
     }
 
